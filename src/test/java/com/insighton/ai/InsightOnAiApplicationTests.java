851091ec7582623f90820e9d89a22950141b88a2
OiJs <oijs9663@gmail.com>
2026-07-23 15:00:17 +0900

    feat: ci dev 추가

--- src/test/java/com/insighton/ai/InsightOnAiApplicationTests.java
+++ src/test/java/com/insighton/ai/InsightOnAiApplicationTests.java
@@ -3,7 +3,9 @@ package com.insighton.ai;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.ai.model.chat=none"
+})
 class InsightOnAiApplicationTests {
 
     @Test
